97dba5c4d4471aa4253f1c0332889a69c6555591
shonac54 <shonac369@gmail.com>
2024-03-02 15:22:05 +0530

    product order and payments

--- gizmoproject/settings.py
+++ gizmoproject/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'accounts',
     'store',
     'carts',
+    'orders',
 ]
 
 MIDDLEWARE = [
--- gizmoproject/urls.py
+++ gizmoproject/urls.py
@@ -27,5 +27,8 @@ urlpatterns = [
     path('accounts/',include('accounts.urls')),
     path('category/' ,include('category.urls')),
 
+    #orders
+    path('orders/',include('orders.urls')),
+
 
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
